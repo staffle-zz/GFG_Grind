ec5c4c3ab58f0c4910be1a2468672958bf9e31fe
Amit Dwivedi <amit.dwivedi.nit@gmail.com>
2020-07-17 22:23:10 +0530

    Update Missing_number_in_array.py

--- Missing_number_in_array.py
+++ Missing_number_in_array.py
@@ -1,10 +1,15 @@
 for _ in range(int(input())):
-    n = int(input()) 
-    A = list(map(int,input().split()))
-    A.sort()
-    for i in range(1,n+1):
-        if(i==n):
-            print(n)
-        elif(i!=A[i-1]):
-            print(i)
-            break
+    n = int(input())
+    A = sum(list(map(int,input().split())))
+    print(((n)*(n+1))//2 - A)
+
+# for _ in range(int(input())):
+#     n = int(input()) 
+#     A = list(map(int,input().split()))
+#     A.sort()
+#     for i in range(1,n+1):
+#         if(i==n):
+#             print(n)
+#         elif(i!=A[i-1]):
+#             print(i)
+#             break
