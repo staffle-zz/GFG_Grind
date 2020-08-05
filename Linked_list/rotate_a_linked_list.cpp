189c6bff2843b6b018c5e6439aef9c101919e0e0
Amit Dwivedi <amit.dwivedi.nit@gmail.com>
2020-08-05 16:25:02 +0530

    Create rotate_a_linked_list.cpp

---
+++ Linked_list/rotate_a_linked_list.cpp
@@ -0,0 +1,14 @@
+Node* rotate(Node* head, int k)
+{
+    Node* temp = head;
+    while(temp->next!=NULL)
+        temp = temp->next;
+    temp->next = head;
+    Node* end;
+    while(k--){
+        end = head;
+        head = head->next;
+    }
+    end->next = NULL;
+    return head;
+}
